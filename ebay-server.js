d03034ae6471df8aab4adbe6822236ad6aeb2604
Data Entry User <Data Entry User>
2016-04-27 12:28:20 -0400

    with response parsing into a console table. initiated express server setup

--- ebay-server.js
+++ ebay-server.js
@@ -1,15 +1,83 @@
+// ========================================================================================
+// DEPENDENCIES
+// Various npm packages that we use to give our server useful functionality
+//=========================================================================================
+
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var colors = require('colors');
 var ebay = require('ebay-api');
 var fs = require('fs');
+var Table = require('cli-table');
 var prKeys = require('./pr_keys.js');
 
-console.log(prKeys.applicationKeys_PR.authToken);
 
+// =========================================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties of our express server
+// =========================================================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// use for downloading files from File Exchange
 var url = 'https://bulksell.ebay.com/ws/eBayISAPI.dll?FileExchangeProgrammaticDownload';
 
-fs.readFile('filex_token.txt', 'utf8', function(err, data){
-	if (err) throw err;
-	console.log('File Exchange Token: ' + data);
+
+//use for reading File Exchange Token
+// fs.readFile('filex_token.txt', 'utf8', function(err, data){
+// 	if (err) throw err;
+// 	console.log('File Exchange Token: ' + data);
+// });
+
+var table = new Table({
+	head: ['Ebay Item ID', 'Title', 'Price'],
+	colWidths: [15, 80, 10]
 });
 
 
+
+var params = {
+  keywords: process.argv[3],
+
+  // add additional fields
+  outputSelector: ['AspectHistogram'],
+
+  paginationInput: {
+    entriesPerPage: 20
+  },
+
+  itemFilter: [
+    {name: 'Seller', value: 'ultrarevparts'},
+    {name: 'MaxPrice', value: '1000.00'}
+  ],
+
+  primaryCategory: [
+    {name: 'categoryName', value: process.argv[2]}
+  ]
+};
+
+ebay.xmlRequest({
+    serviceName: 'Finding',
+    opType: 'findItemsByKeywords',
+    appId: prKeys.applicationKeys_PR.authToken,      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+    params: params,
+    parser: ebay.parseResponseJson    // (default)
+  },
+  // gets all the items together in a merged array
+  function itemsCallback(error, itemsResponse) {
+    if (error) throw error;
+
+    var items = itemsResponse.searchResult.item;
+
+    console.log('Found', items.length, 'items');
+    //console.log(items);
+    
+    for (var i = 0; i < items.length; i++) {
+      table.push([items[i].itemId, items[i].title, '$' + items[i].sellingStatus.convertedCurrentPrice.amount.toFixed(2)]);
+      //console.log(items[i].shippingInfo.shippingServiceCost);
+    }
+    console.log(colors.green(table.toString()));
+  }
+);
