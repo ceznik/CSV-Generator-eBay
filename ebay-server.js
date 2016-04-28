67d0e551c712325b8b3876765d35a1c89074581a
Data Entry User <Data Entry User>
2016-04-28 16:54:07 -0400

    search from html hits eBay

--- ebay-server.js
+++ ebay-server.js
@@ -42,62 +42,62 @@ var url = 'https://bulksell.ebay.com/ws/eBayISAPI.dll?FileExchangeProgrammaticDo
 // });
 
 
-// use for displaying query data to the console
-// var table = new Table({
-// 	head: ['Ebay Item ID', 'Title', 'Price'],
-// 	colWidths: [15, 80, 10]
-// });
+//use for displaying query data to the console
+var table = new Table({
+	head: ['Item ID', 'Title', 'Price'],
+	colWidths: [15, 80, 10]
+});
 
 
 
-var params = {
-  keywords: process.argv[3],
-
-  // add additional fields
-  outputSelector: ['AspectHistogram'],
-
-  paginationInput: {
-    entriesPerPage: 20
-  },
-
-  itemFilter: [
-    {name: 'Seller', value: 'ultrarevparts'}
-  ],
-
-  primaryCategory: [
-    {name: 'categoryName', value: process.argv[2]}
-  ]
-};
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname + '/compsearch.html'));
 });
 
 
-app.post('/', function(req, res){
-	console.log(req.query.ultrarevSearchCriteria);
+app.post('/:name', function(req, res){
+	console.log(req.params.name);
+	var params = {
+	  keywords: req.params.name,
+
+	  // add additional fields
+	  outputSelector: ['AspectHistogram'],
+
+	  paginationInput: {
+	    entriesPerPage: 20
+	  },
+
+	  itemFilter: [
+	    {name: 'Seller', value: 'ultrarevparts'}
+	  ],
+
+	  primaryCategory: [
+	    {name: 'categoryName', value: ''}
+	  ]
+	};
+	ebay.xmlRequest({
+	    serviceName: 'Finding',
+	    opType: 'findItemsByKeywords',
+	    appId: prKeys.applicationKeys_PR.authToken,      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+	    params: params,
+	    parser: ebay.parseResponseJson    // (default)
+	  },
+	  // gets all the items together in a merged array
+	  function itemsCallback(error, itemsResponse) {
+	    if (error) throw error;
+
+	    var items = itemsResponse.searchResult.item;
+
+	    //console.log(colors.magenta.bold('Found', items.length, 'items'));
+	    //console.log(items);
+	    
+	    for (var i = 0; i < items.length; i++) {
+	      table.push([items[i].itemId, items[i].title, '$' + items[i].sellingStatus.convertedCurrentPrice.amount.toFixed(2)]);
+	      //console.log(items[i].shippingInfo.shippingServiceCost);
+	    }
+	    //console.log(colors.green(table.toString()));
+	});
+	console.log(res.send(table));
 });
 
 
-ebay.xmlRequest({
-    serviceName: 'Finding',
-    opType: 'findItemsByKeywords',
-    appId: prKeys.applicationKeys_PR.authToken,      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
-    params: params,
-    parser: ebay.parseResponseJson    // (default)
-  },
-  // gets all the items together in a merged array
-  function itemsCallback(error, itemsResponse) {
-    if (error) throw error;
-
-    var items = itemsResponse.searchResult.item;
-
-    console.log(colors.magenta.bold('Found', items.length, 'items'));
-    //console.log(items);
-    
-    for (var i = 0; i < items.length; i++) {
-      //table.push([items[i].itemId, items[i].title, '$' + items[i].sellingStatus.convertedCurrentPrice.amount.toFixed(2)]);
-      //console.log(items[i].shippingInfo.shippingServiceCost);
-    }
-    //console.log(colors.green(table.toString()));
-  }
-);
