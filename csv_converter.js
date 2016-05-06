b08912defba7ac1dc34e4cd7cc909ec2e865b013
Data Entry User <Data Entry User>
2016-05-06 16:06:33 -0400

    csv processing test file

--- csv_converter.js
+++ csv_converter.js
@@ -20,6 +20,7 @@ fs.readFile(filePath, {
       if (err) {
         console.log(err);
       } else {
+        var columns = data[0].length;
         for (i=0; i<1; i++){
           console.log(data[i].length);
           //console.log(data[1]);
